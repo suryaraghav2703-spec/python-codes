8233c09111c6478a1ef6dbdbe9662fb2ac38254a
surya pratap <suryaraghav2703@gmail.com>
2026-03-18 09:48:01 +0530

    Add files via upload

---
+++ List and Tuple/tuple.py
@@ -0,0 +1,8 @@
+a = (1,2,5,6, False, "surya")
+print(a)
+print(type(a))
+
+b = (1,)
+print(type(b)) # if you want a single value tuple then you have to put comma(,) after it otherwise it will think that the data might be str,int,bool etc.
+
+# you cannot change a tuple you can make a new tuple but can'nt change it coz it is immmutable. tuple is like list but cannot be change
