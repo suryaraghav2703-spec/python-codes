49b4c0fc7f2be10842d75a086f8ddbe61cc2fbcd
surya pratap <suryaraghav2703@gmail.com>
2026-03-19 09:36:30 +0530

    Add files via upload

---
+++ dictionary & sets/dict.py
@@ -0,0 +1,15 @@
+d = {}#empty dictionary
+marks = {
+    "surya" : 100,
+    "shubham" : 56,
+    "kunal" : 23 
+}
+
+# print(marks, type(marks))
+print(marks["surya"]) #dictionary is a collection of keys-value pairs.
+
+#properties
+#1.) it is unordered
+#2.) it is mutable
+#3.) it is indexed
+#4.) cannot contain duplicate keys 
