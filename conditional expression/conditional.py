78f1e4dff7591b0556262c890bb36301c3936471
surya pratap <suryaraghav2703@gmail.com>
2026-03-20 12:42:09 +0530

    Add files via upload

---
+++ conditional expression/conditional.py
@@ -0,0 +1,14 @@
+a = int(input("enter your age: "))
+
+#if else statement
+
+if(a>=18):
+    print("you are above the age of consent")
+    print("good for you")
+
+else:
+    print("you are below the age of consent")    
+
+print("end of program")
+
+# if and else are comditional expression
