9efa73ae0d0a7c07f3a7270501c6790da405f99f
surya pratap <suryaraghav2703@gmail.com>
2026-03-23 09:12:24 +0530

    Add files via upload

---
+++ Function & Recurssion/funtions_with_arguments.py
@@ -0,0 +1,13 @@
+def good_day(name, ending):
+    print("GOOD DAY ," + name)
+    print(ending)
+    return "" # return function ko command dega ki harr variable ka output de.
+good_day("surya", "thank you") 
+good_day("kunal", "thnx")
+good_day("manav", "regards")
+    
+
+a = good_day("arnav", "sup")
+print(a)
+
+
