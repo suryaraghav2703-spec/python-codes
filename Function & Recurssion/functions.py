3757b6dd0d245513fd1b8fb7e00922df8fdf3b54
surya pratap <suryaraghav2703@gmail.com>
2026-03-23 09:12:05 +0530

    Add files via upload

---
+++ Function & Recurssion/functions.py
@@ -0,0 +1,32 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+# average = (a + b + c)/3
+# print(average)
+
+#IF TUMHE YAHI TASK 50 BAAR KARNA PADE TO YE BAHUT BADA OR DIFFICULT CODE HOJAYEGA
+#ISLIYE HUM "FUNCTIONS" KA USE KARTE HAI, TAAKI YE PIECE OF KNOWLEDGE KO EK NAAM MILJAYE.
+
+# FUNCTION DEFINATION 
+def avg():
+    a = int(input("Enter your name: "))
+    b = int(input("Enter your name: "))
+    c = int(input("Enter your name: "))
+    average = (a + b + c)/3
+    print(average)
+
+# FUNCTION CALL
+avg() # ab mai jitni baar ye type karunga utni baar code run hoga 50 times type 50 code runs.
+
+#Quick Quiz: Write a program to greet a user with "Goog day" using functions.
+def good_day():
+    print("GOOD DAY")
+
+good_day()  
+
+#TYPES OF FUNCTIONS IN PYTHON
+#There are two types of functions in python:
+#• Built in functions (Already present in python)
+#• User defined functions (Defined by the user)
+#Examples of built in functions includes len(), print), range) etc.
+#The func1() function we defined is an example of user defined function.
