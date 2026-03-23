3aeebd671760cf963a56266d1f3c0971035bae88
surya pratap <suryaraghav2703@gmail.com>
2026-03-23 09:12:46 +0530

    Add files via upload

---
+++ Function & Recurssion/default_argument.py
@@ -0,0 +1,9 @@
+def good_day(name, ending = "thank you"):
+    print(f"GOOD DAY , {name}")# chahe "f" use karo ya fir "+" same hai .
+    print(ending)
+
+good_day("surya", "thnx")
+good_day("kunal")# ab maine yaha ending value nahi di, to jo maine by deafult value, 
+# "thank you" put kiya hai vo run hogi.
+
+
